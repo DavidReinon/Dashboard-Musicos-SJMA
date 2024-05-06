bb9afe0c0502014fec4d1e350f88824092148ae4
DavidReinon <davidreinongarcia@gmail.com>
2024-05-06 11:22:13 +0200

    Data management without header option in process

--- APP/components/CSVImporter.tsx
+++ APP/components/CSVImporter.tsx
@@ -9,69 +9,32 @@ const CSVImporter: React.FC = () => {
     const [musicians, setMusicians] = useState<
         Array<{ id: string; name: string }>
     >([]);
+    const [events, setEvents] = useState<
+        Array<{ fecha: string; titulo: string; datos: string[] }>
+    >([]);
 
     useEffect(() => {
-        musiciansFilter();
+        // extractMusiciansData();
+        // extractEventsData();
     }, [data]);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (!file) return;
+    const file = event.target.files?.[0];
+    if (!file) return;
 
-        // Utilizar FileReader para leer el archivo como texto
-        const reader = new FileReader();
-        reader.onload = (e) => {
-            if (e.target?.result) {
-                // Parsear el texto del archivo usando Papa Parse
-                Papa.parse<string[]>(e.target.result.toString(), {
-                    complete: (result: Papa.ParseResult<string[]>) => {
-                        // Los datos se encontrarán en result.data
-                        setData(result.data);
-                        // Los encabezados se encontrarán en result.meta.fields
-                        setHeaders(result.meta.fields ?? null);
-                    },
-                    header: true, // Indica que el archivo CSV tiene una fila de encabezado
-                    delimiter: ";", // Establece el delimitador correcto para tu CSV
-                });
-            }
-        };
-        reader.readAsText(file);
-    };
+    Papa.parse<string[]>(file, {
+        complete: (result: Papa.ParseResult<string[]>) => {
+            setData(result.data);
 
-    const eventsFilter = () => {
-        if (data && headers) {
-            const eventData: { fecha: string; titulo: string; tipo: string; datos: string[] }[] = [];
+            // setHeaders(result.meta.fields ?? null);
+        },
+        header: false, // Indicates that the CSV file has a header row
+        delimiter: ";", // Sets the correct delimiter for your CSV
+    });
+};
+
     
-            // Obtener las filas con los datos de fecha, título y tipo
-            const fechaRow = data[0];
-            const tituloRow = data[1];
-            const tipoRow = data[2];
-    
-            headers.forEach((header: string, headerIndex: number) => {
-                if (headerIndex >= 3) {
-                    // A partir del cuarto valor del encabezado (el primero es 5)
-                    const evento: { fecha: string; titulo: string; tipo: string; datos: string[] } = {
-                        fecha: fechaRow[headerIndex] as string,
-                        titulo: tituloRow[headerIndex] as string,
-                        tipo: tipoRow[headerIndex] as string,
-                        datos: [],
-                    };
-    
-                    // Obtener los datos adicionales del evento
-                    for (let i = 3; i < data.length; i++) {
-                        evento.datos.push(data[i][headerIndex] as string);
-                    }
-    
-                    eventData.push(evento);
-                }
-            });
-    
-            setEvents(eventData);
-        }
-    };
-    
-    
-    const eventsFilter = () => {
+    const extractMusiciansData = () => {
         if (data && headers) {
             const eventData: { id: string; name: string }[] = [];
             const nameRow = data[2]; // Obtener la fila con los nombres de los músicos
@@ -89,8 +52,47 @@ const CSVImporter: React.FC = () => {
 
             setMusicians(eventData);
         }
-    }
-        
+    };
+
+    const extractEventsData = () => {
+        if (data && headers) {
+            const eventData: {
+                fecha: string;
+                titulo: string;
+                datos: string[];
+            }[] = [];
+
+            // Obtener las filas con los datos de fecha, título y tipo
+            const fechaRow = data[0];
+            const tituloRow = data[1];
+            const tipoRow = data[2];
+
+            headers.forEach((header: string, headerIndex: number) => {
+                if (headerIndex >= 3) {
+                    // A partir del cuarto valor del encabezado (el primero es 5)
+                    const evento: {
+                        fecha: string;
+                        titulo: string;
+                        datos: string[];
+                    } = {
+                        fecha: fechaRow[headerIndex] as string,
+                        titulo: tituloRow[headerIndex] as string,
+                        datos: [],
+                    };
+
+                    // Obtener los datos adicionales del evento
+                    for (let i = 3; i < data.length; i++) {
+                        evento.datos.push(data[i][headerIndex] as string);
+                    }
+
+                    eventData.push(evento);
+                }
+            });
+
+            setEvents(eventData);
+        }
+    };
+
 
     return (
         <div>
@@ -98,12 +100,37 @@ const CSVImporter: React.FC = () => {
             <input type="file" onChange={handleFileChange} accept=".csv" />
             <div className="flex-1 my-10">
                 {/* Mostrar los datos del archivo CSV */}
+                {data && (
+                    <table>
+                        <tbody>
+                            {data.map((row, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    <p>{row}</p>
+                                    {/* {row.map((cell, cellIndex) => (
+                                        <td key={cellIndex}>{cell}</td>
+                                    ))} */}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                )}
+
+                {/* Mostrar los encabezados del archivo CSV */}
+
+                {/* {data && (
+                    <div>
+                        <h2>Eventos</h2>
+                        <strong>{events[0].fecha}</strong>
+                        <p>{events[0].titulo}</p>
+                    </div>
+                )}
+
                 {musicians.map((musician, index) => (
                     <div key={index}>
                         <strong>{musician.id}</strong>
                         <p>{musician.name}</p>
                     </div>
-                ))}
+                ))} */}
             </div>
         </div>
     );
