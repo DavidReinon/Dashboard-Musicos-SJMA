eaadc94235f88a3fc17a9a4b13aff585b8ed61cd
DavidReinon <davidreinongarcia@gmail.com>
2024-05-08 17:33:10 +0200

    chore: Add table design with demo musician info

--- APP/app/dashboard/musicians/page.tsx
+++ APP/app/dashboard/musicians/page.tsx
@@ -1,13 +1,84 @@
 //Musicos
-import React from 'react'
+"use client";
+
+import React from "react";
+import {
+    Table,
+    TableHeader,
+    TableBody,
+    TableColumn,
+    TableRow,
+    TableCell,
+    getKeyValue,
+} from "@nextui-org/table";
+
+const rows = [
+    {
+        key: "1",
+        display_name: "Tony Reichert",
+        instrument: "CEO",
+        id: "12",
+    },
+    {
+        key: "2",
+        display_name: "Zoey Lang",
+        instrument: "Technical Lead",
+        id: "24",
+    },
+    {
+        key: "3",
+        display_name: "Jane Fisher",
+        instrument: "Senior Developer",
+        id: "15",
+    },
+    {
+        key: "4",
+        display_name: "William Howard",
+        instrument: "Community Manager",
+        id: "8",
+    },
+];
+
+const columns = [
+    {
+        key: "display_name",
+        label: "NAME",
+    },
+    {
+        key: "instrument",
+        label: "INSTRUMENT",
+    },
+    {
+        key: "id",
+        label: "ID",
+    },
+];
 
 const musiciansScreen = () => {
-  return (
-    <div>
-      <h1 className='text-2xl font-bold'>Informacion Musicos:</h1>
-      
-    </div>
-  )
-}
+    return (
+        <div>
+            <Table aria-label="Example table with dynamic content">
+                <TableHeader columns={columns}>
+                    {(column) => (
+                        <TableColumn key={column.key}>
+                            {column.label}
+                        </TableColumn>
+                    )}
+                </TableHeader>
+                <TableBody items={rows}>
+                    {(item) => (
+                        <TableRow key={item.key}>
+                            {(columnKey) => (
+                                <TableCell>
+                                    {getKeyValue(item, columnKey)}
+                                </TableCell>
+                            )}
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
 
-export default musiciansScreen
+export default musiciansScreen;
