d8c0c7b1e1fe69832b595f0ac3973f17fe341f97
DavidReinon <davidreinongarcia@gmail.com>
2024-05-21 17:43:36 +0200

    fix: Fix Date and Time types, and change component name

--- APP/app/dashboard/selections/create-selection/page.tsx
+++ APP/app/dashboard/selections/create-selection/page.tsx
@@ -2,13 +2,16 @@
 
 import React, { useState } from "react";
 import { Input, DatePicker, TimeInput, Button } from "@nextui-org/react";
-import { DateValue, TimeValue } from "@some-date-library"; // Asegúrate de importar los tipos correctos para DateValue y TimeValue
+import {
+    CalendarDate,
+    Time,
+} from "@internationalized/date"; //Libreria para manejar fechas y horas, la que devuelven los componentes de NextUI
 import { useRouter } from "next/navigation";
 
-const CreatePartitionScreen: React.FC = () => {
+const CreateSelectionScreen: React.FC = () => {
     const [name, setName] = useState<string>("");
-    const [date, setDate] = useState<Date | null>(null);
-    const [time, setTime] = useState<TimeValue | null>(null);
+    const [date, setDate] = useState<CalendarDate | null>(null);
+    const [time, setTime] = useState<Time | null>(null);
     const [totalPrice, setTotalPrice] = useState<number>(0);
     const [numMusicians, setNumMusicians] = useState<number>(0);
     const [otherExpenses, setOtherExpenses] = useState<number>(0);
@@ -34,79 +37,95 @@ const CreatePartitionScreen: React.FC = () => {
     };
 
     return (
-      <>
-      <h1 className="my-5 text-3xl font-bold text-left">Datos de la particion: </h1>
-        <form onSubmit={handleSubmit} className="flex">
-            <div className="flex-col mx-10">
-                <Input
-                    className="m-5"
-                    label="Nombre"
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
-                <DatePicker
-                    className="m-5"
-                    label="Fecha"
-                    value={date}
-                    onChange={(value) => setDate(value)} // 'value' es del tipo DateValue
-                />
-                <TimeInput
-                    className="m-5"
-                    label="Tiempo"
-                    value={time}
-                    onChange={setTime} // 'setTime' acepta valores de tipo TimeValue
-                />
-                <Input
+        <>
+            <h1 className="my-5 text-3xl font-bold text-left">
+                Datos de la particion:
+            </h1>
+            <form onSubmit={handleSubmit} className="flex">
+                <div className="flex-col mx-10">
+                    <Input
+                        isRequired
+                        className="m-5"
+                        label="Nombre"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                    <DatePicker
+                        isRequired
+                        className="m-5"
+                        label="Fecha"
+                        value={date}
+                        onChange={(value) => setDate(value)} // 'value' es del tipo DateValue
+                    />
+                    <TimeInput
+                        isRequired
+                        hourCycle={24}
+                        className="m-5"
+                        label="Hora"
+                        value={time}
+                        onChange={(value) => setTime(value)} // 'setTime' acepta valores de tipo TimeValue
+                    />
+                    <Input
+                        isRequired
                     className="m-5"
                     type="number"
                     label="Precio Total"
-                    value={totalPrice}
-                    onChange={(e) => setTotalPrice(Number(e.target.value))}
-                />
-                <Input
-                    className="m-5"
-                    type="number"
-                    label="Número de Músicos"
-                    value={numMusicians}
-                    onChange={(e) => setNumMusicians(Number(e.target.value))}
-                />
-            </div>
-            <div className="flex-col mx-10">
-                <Input
-                    className="m-5"
-                    type="number"
-                    label="Otros Gastos"
-                    value={otherExpenses}
-                    onChange={(e) => setOtherExpenses(Number(e.target.value))}
-                />
-                <Input
-                    className="m-5"
-                    label="Repertorio"
-                    value={repertoire}
-                    onChange={(e) => setRepertoire(e.target.value)}
-                />
-                <Input
-                    className="m-5"
-                    label="Vestimenta"
-                    value={dressCode}
-                    onChange={(e) => setDressCode(e.target.value)}
-                />
-                <Input
-                    className="m-5"
-                    label="Comentarios"
-                    value={comments}
-                    onChange={(e) => setComments(e.target.value)}
-                />
-                <Button color="danger" className="m-5" onClick={() => router.back()}>
-                    Volver
-                </Button>
-                <Button color="primary" className="m-5" type="submit">
-                    Siguiente
-                </Button>
-            </div>
-        </form>
+                        value={totalPrice.toString()}
+                        onChange={(e) => setTotalPrice(Number(e.target.value))}
+                    />
+                    <Input
+                        isRequired
+                        className="m-5"
+                        type="number"
+                        label="Número de Músicos"
+                        value={numMusicians.toString()}
+                        onChange={(e) =>
+                            setNumMusicians(Number(e.target.value))
+                        }
+                    />
+                </div>
+                <div className="flex-col mx-10">
+                    <Input
+                        className="m-5"
+                        type="number"
+                        label="Otros Gastos"
+                        value={otherExpenses.toString()}
+                        onChange={(e) =>
+                            setOtherExpenses(Number(e.target.value))
+                        }
+                    />
+                    <Input
+                        className="m-5"
+                        label="Repertorio"
+                        value={repertoire}
+                        onChange={(e) => setRepertoire(e.target.value)}
+                    />
+                    <Input
+                        className="m-5"
+                        label="Vestimenta"
+                        value={dressCode}
+                        onChange={(e) => setDressCode(e.target.value)}
+                    />
+                    <Input
+                        className="m-5"
+                        label="Comentarios"
+                        value={comments}
+                        onChange={(e) => setComments(e.target.value)}
+                    />
+                    <Button
+                        color="danger"
+                        className="m-5"
+                        onClick={() => router.back()}
+                    >
+                        Volver
+                    </Button>
+                    <Button color="primary" className="m-5" type="submit">
+                        Siguiente
+                    </Button>
+                </div>
+            </form>
         </>
     );
 };
 
-export default CreatePartitionScreen;
+export default CreateSelectionScreen;
